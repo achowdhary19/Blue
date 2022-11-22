815a619340ebb032b0f4bf003b48b3928e9dfd22
ZhangZhongzhiNino <zhangzhongzhi1015@gmail.com>
2022-11-22 01:15:41 -0700

    Character animation update
    Add debug mode for animation
    Change run animation

--- Assets/Scripts/Nino/Player/AnimationController.cs
+++ Assets/Scripts/Nino/Player/AnimationController.cs
@@ -6,15 +6,17 @@ public class AnimationController : MonoBehaviour
 {
     [field: SerializeField] Animator animator;
     [field: SerializeField] PlayerController playerController;
-    [field: SerializeField] InputHandeler input;
+    [field: SerializeField] bool debug_Mode;
     void Start()
     {
         TryGetComponent<Animator>(out animator);
         TryGetComponent<PlayerController>(out playerController);
+        debug_Mode = false;
     }
 
     private void FixedUpdate()
     {
+        if (debug_Mode) return;
         if (playerController.isMoving && animator.GetInteger("State") != 1) animator.SetInteger("State", 1);
         if (!playerController.isMoving&& animator.GetInteger("State") != 0) animator.SetInteger("State", 0);
     }
